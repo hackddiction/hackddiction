b2252a472e473b92fc58d76753ba1c3832a49f26
Hackddiction <126502049+hackddiction@users.noreply.github.com>
2025-03-08 00:42:22 +0100

    20250308

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.sidebar.image("logo.png")
+
+home_page = st.Page(
+    page="views/home.py",
+    title="Home",
+    icon=":material/security:",
+    default=True,
+)
+
+blog_page= st.Page(
+    page="views/blog.py",
+    title="Blog",
+    icon=":material/security:",
+)
+
+pg = st.navigation(pages=[home_page, blog_page])
+
+pg.run()
