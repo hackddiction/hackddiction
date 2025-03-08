2832b82cf5244f382bbf24386615ba9c281712bd
Hackddiction <126502049+hackddiction@users.noreply.github.com>
2025-03-08 01:35:39 +0100

    20250308

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ home_page = st.Page(
 blog_page = st.Page(
     page="views/blog.py",
     title="Blog",
-    icon=":material/security:",
+    icon=":material/note_stack:",
 )
 
-pg = st.navigation(pages=[home_page, blog_page])
+pg = st.navigation(pages=[home_page, blog_page], expanded=True)
 
 pg.run()
